39a3eade67578ab48148b6efbed780340e239b2f
Parminder <parmindersingh1086@gmail.com>
2023-07-13 21:25:34 -0400

    setup location of components in the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { AccountListComponent } from './account-list/account-list.component';
+import { AccountListComponent } from './dashboard/account-list/account-list.component';
 import { AccountComponent } from './dashboard/account-list/account/account.component';
 import { TransactionListComponent } from './dashboard/account-list/account/transaction-list/transaction-list.component';
 import { TransactionComponent } from './dashboard/account-list/account/transaction-list/transaction/transaction.component';

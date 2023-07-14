c74be046e1ba859d2c69ec6a8ad4cecdc93fe6ba
Parminder <parmindersingh1086@gmail.com>
2023-07-13 20:47:24 -0400

    Added a few starting components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AccountListComponent } from './account-list/account-list.component';
+import { AccountComponent } from './dashboard/account-list/account/account.component';
+import { TransactionListComponent } from './dashboard/account-list/account/transaction-list/transaction-list.component';
+import { TransactionComponent } from './dashboard/account-list/account/transaction-list/transaction/transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    AccountListComponent,
+    AccountComponent,
+    TransactionListComponent,
+    TransactionComponent
   ],
   imports: [
     BrowserModule
